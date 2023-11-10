ed5ebd283ea1c5438e53adb4395c585fb097524e
kat4eri <kat4eri@gmail.com>
2023-11-10 17:16:41 +0300

    done

--- script.js
+++ script.js
@@ -1,71 +1,65 @@
-1.let strJs = 'js';
-strJs = strJs.toUpperCase();
+1.const people = [
+  { name: 'Глеб', age: 29 },
+  { name: 'Анна', age: 17 },
+  { name: 'Олег', age: 7 },
+  { name: 'Оксана', age: 47 }
+];
+people.sort((a, b) => a.age - b.age);
+console.log(people);
 
-console.log(strJs);
+2.function isPositive(number) {    
+	return number > 0;  
+}     
+function isMale(person) {   
+	return person.gender ==="male";    
+} 
+	
+function filter(array, ruleFunction) {   
+	const result = [];   
+	for (let item of array) {    
+	  if (ruleFunction(item)){
+		result.push(item);
+	  }
+	}
+	   return result;  
+} 
+console.log(filter([3, -4, 1, 9], isPositive));  
+	
+const people = [ 
+	{name: 'глеб', gender: 'male'},
+	{name: 'анна', gender: 'female'},
+	{name: 'олег', gender: 'male'},
+	{name: 'оксана', gender: 'female'} 
+];
+	
+console.log(filter(people, isMale));
 
-2.const searchStart = (['Кот', 'Корова', 'Рыба', 'Оса']);
-const search = 'ко';
+3.let counter = 0;
+const interval = setInterval ( () => {
+  console.log(new Date());
+  counter += 3;
+  if (counter >= 30) {
+    clearInterval(interval);
+    console.log('30 секунд прошло');
+}
+}, 3000)
 
-searchStart.forEach((searchStart) => {
-    if (searchStart.toLowerCase().startsWith(search.toLowerCase())){
-        console.log(Array(searchStart));
-    }
+4.function delayForSecond(callback) {
+  setTimeout(callback, 1000);
+}
+delayForSecond(function () {
+  console.log('Привет, Глеб!');
 });
 
-3.let number = 32.58884;
-
-console.log(Math.floor(number));
-console.log(Math.ceil(number));
-console.log(Math.round(number));
-
-4.console.log(Math.max(52, 53, 49, 77, 21, 32));
-console.log(Math.min(52, 53, 49, 77, 21, 32));
-
-5.function getRandom(minValue, maxValue) {
-  return Math.round (Math.random() * (maxValue - minValue)) + minValue;
-};
-
-console.log(getRandom(1, 10));
-
-6.let max = 10;
-let lenght = max / 2;
-const getRandomArrNumbers = (length, max) => (
-    [...new Array(length)]
-      .map(() => Math.round(Math.random() * max))
-  );
-const numbers = getRandomArrNumbers (lenght, max);
-
-console.log(numbers);
-
-7.function getRandomNumber(minNum, maxNum) {
-  return Math.round (Math.random() * (maxNum - minNum)) + minNum;
-};
-
-console.log(getRandomNumber(3, 13));
-
-8.let nowDate = new Date();
-
-console.log(nowDate);
-
-9.let myDate = new Date();
-let searchDate = myDate.setDate(myDate.getDate() + 73);
-let daysIn73 = new Date(searchDate)
-console.log(daysIn73);
-
-10.const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь','Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-
-let newDate = new Date();
-let fullDate = 'Дата: ' + newDate.getDate() + ' ' + months[newDate.getMonth()] + ' ' + newDate.getFullYear() + '- это ' + days[newDate.getDay()];
-console.log(fullDate);
-
-let hour = newDate.getHours();
-let minute = newDate.getMinutes();
-let second = newDate.getSeconds();
-if (minute < 10) {
-	minute = '0' + minute;
-};
-if (second < 10) {
-	second = '0' + second;
-};
-console.log('Время: ' + hour + ':' + minute + ':' + second);
+5.function delayForSecond(cb) {
+	setTimeout(() => {
+	  console.log('Прошла одна секунда');
+		  if(cb) {
+		cb();
+	  }
+	}, 1000);
+}
+function sayHi (name) {
+	console.log(`Привет, ${name}!`);
+}
+delayForSecond(() => sayHi('Глеб'));
